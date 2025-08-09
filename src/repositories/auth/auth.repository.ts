430cbc9c531a44f56589bb4bffdb6815f35986db
Rasool <rasoolol.77@gmail.com>
2025-08-09 22:01:43 +0330

    fix: change auth repository from class to object

--- src/repositories/auth/auth.repository.ts
+++ src/repositories/auth/auth.repository.ts
@@ -6,30 +6,30 @@ import {
   User,
 } from '@/types';
 
-class AuthRepository {
-  async login(credentials: LoginRequestPayload): Promise<LoginResponse> {
-    const response = await api.post('/auth/signin', credentials);
+const authRepository = {
+  login: async (payload: LoginRequestPayload): Promise<LoginResponse> => {
+    const response = await api.post('/auth/signin', payload);
     return response.data;
-  }
+  },
 
-  async register(credentials: registerRequestPayload): Promise<LoginResponse> {
-    const response = await api.post('/auth/signup', credentials);
+  register: async (payload: registerRequestPayload): Promise<LoginResponse> => {
+    const response = await api.post('/auth/signup', payload);
     return response.data;
-  }
+  },
 
-  async getCurrentUser(): Promise<User> {
+  getCurrentUser: async (): Promise<User> => {
     const response = await api.get('/auth/me');
     return response.data;
-  }
+  },
 
-  async logout() {
+  logout: async () => {
     await api.post('/auth/logout');
-  }
+  },
 
-  async refreshToken(refreshToken: string) {
+  refreshToken: async (refreshToken: string) => {
     //TODO implement this later
     return { accessToken: '', refreshToken };
-  }
-}
+  },
+};
 
-export const authRepository = new AuthRepository();
+export { authRepository };
