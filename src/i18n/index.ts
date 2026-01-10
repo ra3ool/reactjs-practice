c5f0a269cf7a6b181fe732da563285b21a1ca0bf
Rasool <rasoolol.77@gmail.com>
2026-01-10 19:01:26 +0330

    feat: install i18next-browser-languagedetector and add to i18n index file

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,12 +1,12 @@
 import i18n from 'i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import en from './locales/en/common.json';
 import fa from './locales/fa/common.json';
 
 i18n
-  // .use(LanguageDetector)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
