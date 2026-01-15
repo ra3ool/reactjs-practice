d6e8999c21c07d7d9ffb227010f4c18cd3138f51
Rasool <rasoolol.77@gmail.com>
2026-01-15 21:57:36 +0330

    feat: add language constants

---
+++ src/constants/common/common.constants.ts
@@ -0,0 +1,4 @@
+export const LANGUAGES = Object.freeze({
+  fa: 'fa',
+  en: 'en',
+});
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export * from './auth';
+export * from './common';
 export * from './routes';
 
 export * from './aclRules';
