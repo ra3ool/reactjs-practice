b45ea3181318799a531fac5407c124fae847e8b5
Rasool <rasoolol.77@gmail.com>
2026-06-17 17:55:04 +0330

    feat: add public api to comment store

--- src/stores/comments/index.ts
+++ src/stores/comments/index.ts
@@ -1,17 +1,1 @@
-import { create } from 'zustand';
-
-interface CommentUIState {
-  expanded: Record<number, boolean>;
-  toggleExpand: (id: number) => void;
-}
-
-export const useCommentsStore = create<CommentUIState>((set) => ({
-  expanded: {},
-  toggleExpand: (id) =>
-    set((state) => ({
-      expanded: {
-        ...state.expanded,
-        [id]: !state.expanded[id],
-      },
-    })),
-}));
+export { useCommentsStore } from './comment.store';
