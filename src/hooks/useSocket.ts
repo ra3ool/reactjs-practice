a5047d40746a6b501ab2f23f4bca22a0c4948186
Rasool <rasoolol.77@gmail.com>
2026-06-18 16:46:30 +0330

    fix: fix socket hook error

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,7 +1,9 @@
 import { socket } from '@/libs/socket';
+import type {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from '@/types/socket';
 import { useCallback, useEffect, useState } from 'react';
-
-//TODO fix ts errors
 
 export const useSocket = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
@@ -14,37 +16,51 @@ export const useSocket = () => {
       setIsConnected(true);
       setLastError(null);
     };
-
     const onDisconnect = () => setIsConnected(false);
-
-    const onError = (err: unknown) => {
+    const onConnectError = (err: unknown) => {
       setLastError(err instanceof Error ? err : new Error(String(err)));
     };
 
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
-    socket.on('connect_error', onError);
-    socket.on('error', onError);
+    socket.on('connect_error', onConnectError);
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
-      socket.off('connect_error', onError);
-      socket.off('error', onError);
+      socket.off('connect_error', onConnectError);
     };
   }, []);
 
-  const emit = useCallback(<T,>(event: string, payload: T) => {
-    socket.emit(event, payload);
-  }, []);
+  const emit = useCallback(
+    <Event extends keyof ClientToServerEvents>(
+      event: Event,
+      ...args: Parameters<ClientToServerEvents[Event]>
+    ) => {
+      socket.emit(event, ...args);
+    },
+    [],
+  );
 
-  const on = useCallback(<T,>(event: string, callback: (data: T) => void) => {
-    socket.on(event, callback);
-  }, []);
+  const on = useCallback(
+    <Event extends keyof ServerToClientEvents>(
+      event: Event,
+      callback: ServerToClientEvents[Event],
+    ) => {
+      socket.on(event, callback as never);
+    },
+    [],
+  );
 
-  const off = useCallback(<T,>(event: string, callback: (data: T) => void) => {
-    socket.off(event, callback);
-  }, []);
+  const off = useCallback(
+    <Event extends keyof ServerToClientEvents>(
+      event: Event,
+      callback: ServerToClientEvents[Event],
+    ) => {
+      socket.off(event, callback as never);
+    },
+    [],
+  );
 
   return { isConnected, emit, on, off, lastError };
 };
